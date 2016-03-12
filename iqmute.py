fe65fb4bdc3266fa8d2330d54000c54530cef029
Philippe Fernandez <mahagony4@gmail.com>
2016-03-12 18:00:14 +0000

    add --show to get the MUTE state of the Pi-DigiAMP+

--- iqmute.py
+++ iqmute.py
@@ -6,6 +6,7 @@ import RPi.GPIO as GPIO
 class IQaudIO:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
         self.port = 22
         GPIO.setup(self.port, GPIO.OUT)
 
@@ -18,14 +19,26 @@ class IQaudIO:
     def unmute(self):
         self.output(1)
 
+    def show(self):
+        if GPIO.input(self.port):
+            print("Pi-DigiAMP+ is in UNMUTE state")
+        else:
+            print("PI-DigiAMP+ is in MUTE state")
+
     def __del__(self):
         GPIO.cleanup()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="mute/unmute IQAudIO Pi-DigiAMP+")
-    parser.add_argument("--mute", action="store_true", help="umute by default")
+    parser.add_argument("--mute", action="store_true", help="unmute by default")
+    parser.add_argument("--show", action="store_true", help="show status")
     args = parser.parse_args()
+    if args.show:
+        IQaudIO().show()
+        exit()
     if args.mute:
         IQaudIO().mute()
+        exit()
     else:
         IQaudIO().unmute()
+        exit()
